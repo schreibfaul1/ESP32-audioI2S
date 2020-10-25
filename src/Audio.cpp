ed6ad8251fcc7ee0ee528f515e537bddfa4b11f9
Cellie <CelliesProjects@users.noreply.github.com>
2020-10-25 15:32:34 +0100

    Make arg const.

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -2021,7 +2021,7 @@ bool Audio::setPinout(uint8_t BCLK, uint8_t LRC, uint8_t DOUT, int8_t DIN){
       .data_out_num = m_DOUT,
       .data_in_num = m_DIN
     };
-    esp_err_t result = i2s_set_pin((i2s_port_t)m_i2s_num, &pins);
+    const esp_err_t result = i2s_set_pin((i2s_port_t)m_i2s_num, &pins);
     return (result == ESP_OK);
 }
 //---------------------------------------------------------------------------------------------------------------------
