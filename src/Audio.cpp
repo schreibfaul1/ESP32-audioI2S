67264b7e5027282368f1c0af16ee0e0b0c088948
Wolle <schreibfaul1@t-online.de>
2022-08-23 20:54:06 +0200

    fix packets lost in HLS-TS #334

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -3,8 +3,8 @@
  *
  *  Created on: Oct 26.2018
  *
- *  Version 2.0.5j
- *  Updated on: Aug 21.2022
+ *  Version 2.0.5k
+ *  Updated on: Aug 23.2022
  *      Author: Wolle (schreibfaul1)
  *
  */
@@ -309,6 +309,7 @@ void Audio::setDefaults() {
     clientsecure.flush();
     _client = static_cast<WiFiClient*>(&client); /* default to *something* so that no NULL deref can happen */
     playI2Sremains();
+    ts_parsePacket(0, 0, 0); // reset ts routine
 
     AUDIO_INFO("buffers freed, free Heap: %u bytes", ESP.getFreeHeap());
 
@@ -3154,7 +3155,6 @@ void Audio::processWebStreamTS() {
         tmr_1s = millis();
         m_t0 = millis();
         ts_packetPtr = 0;
-        ts_parsePacket(0, 0, 0); // reset ts routine
         m_controlCounter = 0;
         m_f_firstCall = false;
     }
@@ -4814,7 +4814,9 @@ bool Audio::ts_parsePacket(uint8_t* packet, uint8_t* packetStart, uint8_t* packe
         *packetLength = 0;
         return true;
     }
-    if(m_f_Log) log_e("invalid ts packet!");
+    // PES received before PAT and PMT seen
+    *packetStart = 0;
+    *packetLength = 0;
     return false;
 }
 //----------------------------------------------------------------------------------------------------------------------
--- src/Audio.h
+++ src/Audio.h
@@ -2,7 +2,7 @@
  * Audio.h
  *
  *  Created on: Oct 28,2018
- *  Updated on: Aug 21,2022
+ *  Updated on: Aug 23,2022
  *      Author: Wolle (schreibfaul1)
  */
 
