04ff609d50d12b40929e28958bbc6a2e1b3e2d6b
Wolle <schreibfaul1@t-online.de>
2023-04-08 07:42:56 +0200

    prepare for VORBIS

--- src/vorbis_decoder/vorbis_decoder.h
+++ src/vorbis_decoder/vorbis_decoder.h
@@ -6,6 +6,16 @@
 #pragma once
 #include <stdint.h>
 
+#define ERR_VORBIS_NONE 0
+#define ERR_VORBIS_CHANNELS_OUT_OF_RANGE 1
+#define ERR_VORBIS_INVALID_SAMPLERATE 2
+#define ERR_VORBIS_EXTRA_CHANNELS_UNSUPPORTED 3
+#define ERR_VORBIS_DECODER_ASYNC 4
+#define ERR_VORBIS_OGG_SYNC_NOT_FOUND 5
+#define ERR_VORBIS_BAD_HEADER 6
+#define ERR_VORBIS_NOT_AUDIO 7
+#define ERR_VORBIS_BAD_PACKET 8
+
 
 
 void VORBISDecoder_FreeBuffers(){;}
@@ -17,3 +27,4 @@ int  VORBISGetBitRate(){return 0;}
 int  VORBISDecode(uint8_t* data, int* bytesLeft, int16_t* m_outBuff){return 0;}
 int  VORBISGetOutputSamps(){return 0;}
 char* VORBISgetStreamTitle(){return nullptr;}
+int  VORBISGetChannels(){return 0;}
