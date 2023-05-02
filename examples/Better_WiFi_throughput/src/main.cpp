0f2c1ad10a74fe8307927071dfd9f4cf9883fe71
Wolle <schreibfaul1@t-online.de>
2023-05-02 20:49:42 +0200

    better WiFi speed

---
+++ examples/Better_WiFi_throughput/src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include "WiFiMulti.h"
+#include "Audio.h"
+
+Audio audio;
+WiFiMulti wifiMulti;
+
+String ssid =     "xxxx";
+String password = "xxxx";
+
+#define I2S_LRC     26
+#define I2S_DOUT    25
+#define I2S_BCLK    27
+#define I2S_MCLK     0
+
+void setup() {
+    Serial.begin(115200);
+    wifiMulti.addAP(ssid.c_str(), password.c_str());
+    wifiMulti.run();
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT, -1, -1);
+    audio.setVolume(20); // 0...21
+    audio.connecttohost("http://us3.internet-radio.com:8342/stream");
+}
+
+void loop(){
+    audio.loop();
+}
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
