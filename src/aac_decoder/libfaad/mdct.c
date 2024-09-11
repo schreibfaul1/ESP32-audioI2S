c645ae4eb4be1c903881197e1e84593490788c71
schreibfaul1 <schreibfaul1@t-online.de>
2024-09-11 19:06:10 +0200

    housekeeping

--- src/aac_decoder/libfaad/mdct.c
+++ src/aac_decoder/libfaad/mdct.c
@@ -12,14 +12,11 @@
 #include "cfft.h"
 #include "mdct.h"
 #include "mdct_tab.h"
-
+//————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 mdct_info* faad_mdct_init(uint16_t N) {
     mdct_info* mdct = (mdct_info*)faad_malloc(sizeof(mdct_info));
-
     assert(N % 8 == 0);
-
     mdct->N = N;
-
     /* NOTE: For "small framelengths" in FIXED_POINT the coefficients need to be
      * scaled by sqrt("(nearest power of 2) > N" / N) */
 
@@ -52,26 +49,22 @@ mdct_info* faad_mdct_init(uint16_t N) {
     mdct->cycles = 0;
     mdct->fft_cycles = 0;
 #endif
-
     return mdct;
 }
-
+//————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 void faad_mdct_end(mdct_info* mdct) {
     if(mdct != NULL) {
 #ifdef PROFILE
         printf("MDCT[%.4d]:         %I64d cycles\n", mdct->N, mdct->cycles);
         printf("CFFT[%.4d]:         %I64d cycles\n", mdct->N / 4, mdct->fft_cycles);
 #endif
-
         cfftu(mdct->cfft);
-
         faad_free(mdct);
     }
 }
-
+//————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 void faad_imdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     uint16_t k;
-
     complex_t x;
 #ifdef ALLOW_SMALL_FRAMELENGTH
     #ifdef FIXED_POINT
@@ -80,16 +73,13 @@ void faad_imdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
 #endif
     ALIGN complex_t Z1[512];
     complex_t*      sincos = mdct->sincos;
-
     uint16_t N = mdct->N;
     uint16_t N2 = N >> 1;
     uint16_t N4 = N >> 2;
     uint16_t N8 = N >> 3;
-
 #ifdef PROFILE
     int64_t count1, count2 = faad_get_ts();
 #endif
-
 #ifdef ALLOW_SMALL_FRAMELENGTH
     #ifdef FIXED_POINT
     /* detect non-power of 2 */
@@ -101,27 +91,21 @@ void faad_imdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     }
     #endif
 #endif
-
     /* pre-IFFT complex multiplication */
     for(k = 0; k < N4; k++) { ComplexMult(&IM(Z1[k]), &RE(Z1[k]), X_in[2 * k], X_in[N2 - 1 - 2 * k], RE(sincos[k]), IM(sincos[k])); }
-
 #ifdef PROFILE
     count1 = faad_get_ts();
 #endif
-
     /* complex IFFT, any non-scaling FFT can be used here */
     cfftb(mdct->cfft, Z1);
-
 #ifdef PROFILE
     count1 = faad_get_ts() - count1;
 #endif
-
     /* post-IFFT complex multiplication */
     for(k = 0; k < N4; k++) {
         RE(x) = RE(Z1[k]);
         IM(x) = IM(Z1[k]);
         ComplexMult(&IM(Z1[k]), &RE(Z1[k]), IM(x), RE(x), RE(sincos[k]), IM(sincos[k]));
-
 #ifdef ALLOW_SMALL_FRAMELENGTH
     #ifdef FIXED_POINT
         /* non-power of 2 MDCT scaling */
@@ -132,49 +116,38 @@ void faad_imdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     #endif
 #endif
     }
-
     /* reordering */
     for(k = 0; k < N8; k += 2) {
         X_out[2 * k] = IM(Z1[N8 + k]);
         X_out[2 + 2 * k] = IM(Z1[N8 + 1 + k]);
-
         X_out[1 + 2 * k] = -RE(Z1[N8 - 1 - k]);
         X_out[3 + 2 * k] = -RE(Z1[N8 - 2 - k]);
-
         X_out[N4 + 2 * k] = RE(Z1[k]);
         X_out[N4 + +2 + 2 * k] = RE(Z1[1 + k]);
-
         X_out[N4 + 1 + 2 * k] = -IM(Z1[N4 - 1 - k]);
         X_out[N4 + 3 + 2 * k] = -IM(Z1[N4 - 2 - k]);
-
         X_out[N2 + 2 * k] = RE(Z1[N8 + k]);
         X_out[N2 + +2 + 2 * k] = RE(Z1[N8 + 1 + k]);
-
         X_out[N2 + 1 + 2 * k] = -IM(Z1[N8 - 1 - k]);
         X_out[N2 + 3 + 2 * k] = -IM(Z1[N8 - 2 - k]);
-
         X_out[N2 + N4 + 2 * k] = -IM(Z1[k]);
         X_out[N2 + N4 + 2 + 2 * k] = -IM(Z1[1 + k]);
-
         X_out[N2 + N4 + 1 + 2 * k] = RE(Z1[N4 - 1 - k]);
         X_out[N2 + N4 + 3 + 2 * k] = RE(Z1[N4 - 2 - k]);
     }
-
 #ifdef PROFILE
     count2 = faad_get_ts() - count2;
     mdct->fft_cycles += count1;
     mdct->cycles += (count2 - count1);
 #endif
 }
-
+//————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 #ifdef LTP_DEC
 void faad_mdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     uint16_t k;
-
     complex_t       x;
     ALIGN complex_t Z1[512];
     complex_t*      sincos = mdct->sincos;
-
     uint16_t N = mdct->N;
     uint16_t N2 = N >> 1;
     uint16_t N4 = N >> 2;
@@ -185,7 +158,6 @@ void faad_mdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     #else
     real_t scale = REAL_CONST(4.0 / N);
     #endif
-
     #ifdef ALLOW_SMALL_FRAMELENGTH
         #ifdef FIXED_POINT
     /* detect non-power of 2 */
@@ -196,35 +168,26 @@ void faad_mdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     }
         #endif
     #endif
-
     /* pre-FFT complex multiplication */
     for(k = 0; k < N8; k++) {
         uint16_t n = k << 1;
         RE(x) = X_in[N - N4 - 1 - n] + X_in[N - N4 + n];
         IM(x) = X_in[N4 + n] - X_in[N4 - 1 - n];
-
         ComplexMult(&RE(Z1[k]), &IM(Z1[k]), RE(x), IM(x), RE(sincos[k]), IM(sincos[k]));
-
         RE(Z1[k]) = MUL_R(RE(Z1[k]), scale);
         IM(Z1[k]) = MUL_R(IM(Z1[k]), scale);
-
         RE(x) = X_in[N2 - 1 - n] - X_in[n];
         IM(x) = X_in[N2 + n] + X_in[N - 1 - n];
-
         ComplexMult(&RE(Z1[k + N8]), &IM(Z1[k + N8]), RE(x), IM(x), RE(sincos[k + N8]), IM(sincos[k + N8]));
-
         RE(Z1[k + N8]) = MUL_R(RE(Z1[k + N8]), scale);
         IM(Z1[k + N8]) = MUL_R(IM(Z1[k + N8]), scale);
     }
-
     /* complex FFT, any non-scaling FFT can be used here  */
     cfftf(mdct->cfft, Z1);
-
     /* post-FFT complex multiplication */
     for(k = 0; k < N4; k++) {
         uint16_t n = k << 1;
         ComplexMult(&RE(x), &IM(x), RE(Z1[k]), IM(Z1[k]), RE(sincos[k]), IM(sincos[k]));
-
         X_out[n] = -RE(x);
         X_out[N2 - 1 - n] = IM(x);
         X_out[N2 + n] = -IM(x);
@@ -232,3 +195,4 @@ void faad_mdct(mdct_info* mdct, real_t* X_in, real_t* X_out) {
     }
 }
 #endif
+//————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
