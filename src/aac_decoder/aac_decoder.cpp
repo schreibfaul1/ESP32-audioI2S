264deeca7672ed51135bdb6a449e310af125465e
schreibfaul1 <schreibfaul1@t-online.de>
2025-09-19 15:19:56 +0200

    housekeeping

--- src/aac_decoder/aac_decoder.cpp
+++ src/aac_decoder/aac_decoder.cpp
@@ -14,40 +14,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 AACDecoder::AACDecoder(Audio& audioRef)
-    : Decoder(audioRef), audio(audioRef), neaacdec(std::make_unique<NeaacDecoder>()) {}
+    : Decoder(audioRef), audio(audioRef), m_neaacdec(std::make_unique<NeaacDecoder>()) {}
 
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 bool AACDecoder::init() {
-    before = clock();
-    hAac = neaacdec->NeAACDecOpen();
-    conf = neaacdec->NeAACDecGetCurrentConfiguration(hAac);
+    m_hAac = m_neaacdec->NeAACDecOpen();
+    m_conf = m_neaacdec->NeAACDecGetCurrentConfiguration(m_hAac);
 
-    if (hAac) f_decoderIsInit = true;
-    f_firstCall = false;
-    f_setRaWBlockParams = false;
-    return f_decoderIsInit;
+    if (m_hAac) m_f_decoderIsInit = true;
+    m_f_firstCall = false;
+    m_f_setRaWBlockParams = false;
+    return m_f_decoderIsInit;
 }
 void AACDecoder::clear() {
     return; // nothing todo
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 void AACDecoder::reset() {
-    neaacdec->NeAACDecClose(hAac);
-    hAac = NULL;
-    f_decoderIsInit = false;
-    f_firstCall = false;
-    clock_t difference = clock() - before;
-    int     msec = difference / CLOCKS_PER_SEC;
-    (void)msec;
-    //    printf("ms %li\n", difference);
+    m_neaacdec->NeAACDecClose(m_hAac);
+    m_hAac = NULL;
+    m_f_decoderIsInit = false;
+    m_f_firstCall = false;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 bool AACDecoder::isValid() {
-    return f_decoderIsInit;
+    return m_f_decoderIsInit;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 int32_t AACDecoder::findSyncWord(uint8_t* buf, int32_t nBytes) {
     const int MIN_ADTS_HEADER_SIZE = 7;
     auto      validate = [](const uint8_t* buf) -> bool { // check the ADTS header for validity
@@ -79,118 +74,118 @@ int32_t AACDecoder::findSyncWord(uint8_t* buf, int32_t nBytes) {
 
     return -1;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint8_t AACDecoder::getChannels() {
-    return aacChannels;
+    return m_aacChannels;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint32_t AACDecoder::getSampleRate() {
-    return aacSamplerate;
+    return m_aacSamplerate;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint32_t AACDecoder::getOutputSamples() {
-    return validSamples;
+    return m_validSamples;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint8_t AACDecoder::getBitsPerSample() {
     return 16;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint32_t AACDecoder::getBitRate() {
     uint32_t br = getBitsPerSample() * getChannels() * getSampleRate();
-    return (br / compressionRatio);
+    return (br / m_compressionRatio);
     ;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint32_t AACDecoder::getAudioDataStart() {
     return 0; // nothing todo
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 uint32_t AACDecoder::getAudioFileDuration() {
     return 0; // nothing todo
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 const char* AACDecoder::getStreamTitle() {
     return nullptr; // nothing todo
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 const char* AACDecoder::getErrorMessage(int8_t err) {
-    return neaacdec->NeAACDecGetErrorMessage(abs(err));
+    return m_neaacdec->NeAACDecGetErrorMessage(abs(err));
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 int32_t AACDecoder::decode(uint8_t* inbuf, int32_t* bytesLeft, int16_t* outbuf) {
     uint8_t* ob = (uint8_t*)outbuf;
-    if (f_firstCall == false) {
-        if (f_setRaWBlockParams) { // set raw AAC values, e.g. for M4A config.
-            f_setRaWBlockParams = false;
-            conf->defSampleRate = aacSamplerate;
-            conf->outputFormat = FAAD_FMT_16BIT;
-            conf->useOldADTSFormat = 1;
-            conf->defObjectType = 2;
-            int8_t ret = neaacdec->NeAACDecSetConfiguration(hAac, conf);
+    if (m_f_firstCall == false) {
+        if (m_f_setRaWBlockParams) { // set raw AAC values, e.g. for M4A config.
+            m_f_setRaWBlockParams = false;
+            m_conf->defSampleRate = m_aacSamplerate;
+            m_conf->outputFormat = FAAD_FMT_16BIT;
+            m_conf->useOldADTSFormat = 1;
+            m_conf->defObjectType = 2;
+            int8_t ret = m_neaacdec->NeAACDecSetConfiguration(m_hAac, m_conf);
             (void)ret;
 
             uint8_t specificInfo[2];
-            createAudioSpecificConfig(specificInfo, aacProfile, neaacdec->get_sr_index(aacSamplerate), aacChannels);
-            int8_t err = neaacdec->NeAACDecInit2(hAac, specificInfo, 2, &aacSamplerate, &aacChannels);
+            createAudioSpecificConfig(specificInfo, m_aacProfile, m_neaacdec->get_sr_index(m_aacSamplerate), m_aacChannels);
+            int8_t err = m_neaacdec->NeAACDecInit2(m_hAac, specificInfo, 2, &m_aacSamplerate, &m_aacChannels);
             (void)err;
         } else {
-            neaacdec->NeAACDecSetConfiguration(hAac, conf);
-            int8_t err = neaacdec->NeAACDecInit(hAac, inbuf, *bytesLeft, &aacSamplerate, &aacChannels);
+            m_neaacdec->NeAACDecSetConfiguration(m_hAac, m_conf);
+            int8_t err = m_neaacdec->NeAACDecInit(m_hAac, inbuf, *bytesLeft, &m_aacSamplerate, &m_aacChannels);
             (void)err;
         }
-        f_firstCall = true;
+        m_f_firstCall = true;
     }
 
-    neaacdec->NeAACDecDecode2(hAac, &frameInfo, inbuf, *bytesLeft, (void**)&ob, 2048 * 2 * sizeof(int16_t));
-    *bytesLeft -= frameInfo.bytesconsumed;
-    validSamples = frameInfo.samples;
-    int8_t err = 0 - frameInfo.error;
-    compressionRatio = (float)frameInfo.samples * 2 / frameInfo.bytesconsumed;
+    m_neaacdec->NeAACDecDecode2(m_hAac, &m_frameInfo, inbuf, *bytesLeft, (void**)&ob, 2048 * 2 * sizeof(int16_t));
+    *bytesLeft -= m_frameInfo.bytesconsumed;
+    m_validSamples = m_frameInfo.samples;
+    int8_t err = 0 - m_frameInfo.error;
+    m_compressionRatio = (float)m_frameInfo.samples * 2 / m_frameInfo.bytesconsumed;
     if (err < 0) AAC_LOG_INFO(getErrorMessage(abs(err)));
     return err;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 void AACDecoder::setRawBlockParams(uint8_t channels, uint32_t sampleRate, uint8_t dummy1, uint32_t profile, uint32_t dummy2) {
-    f_setRaWBlockParams = true;
-    aacChannels = channels;     // 1: Mono, 2: Stereo
-    aacSamplerate = sampleRate; // 8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
-    aacProfile = profile;       // 1: AAC Main, 2: AAC LC (Low Complexity), 3: AAC SSR (Scalable Sample Rate), 4: AAC LTP (Long Term Prediction)
+    m_f_setRaWBlockParams = true;
+    m_aacChannels = channels;     // 1: Mono, 2: Stereo
+    m_aacSamplerate = sampleRate; // 8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
+    m_aacProfile = profile;       // 1: AAC Main, 2: AAC LC (Low Complexity), 3: AAC SSR (Scalable Sample Rate), 4: AAC LTP (Long Term Prediction)
     return;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 std::vector<uint32_t> AACDecoder::getMetadataBlockPicture() {
     std::vector<uint32_t> a;
     return a;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 const char* AACDecoder::arg1() { // AAC format
     m_arg1.assign("AAC HeaderFormat: ");
-    if (frameInfo.header_type == 0) m_arg1.append("RAW");
-    if (frameInfo.header_type == 1) m_arg1.append("ADIF"); /* single ADIF header at the beginning of the file */
-    if (frameInfo.header_type == 2) m_arg1.append("ADTS"); /* ADTS header at the beginning of each frame */
+    if (m_frameInfo.header_type == 0) m_arg1.append("RAW");
+    if (m_frameInfo.header_type == 1) m_arg1.append("ADIF"); /* single ADIF header at the beginning of the file */
+    if (m_frameInfo.header_type == 2) m_arg1.append("ADTS"); /* ADTS header at the beginning of each frame */
     return m_arg1.c_get();
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 const char* AACDecoder::arg2() {
     return "arg2";
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 int32_t AACDecoder::val1() { //Parametric Stereo
-    return frameInfo.isPS;
+    return m_frameInfo.isPS;
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 int32_t AACDecoder::val2() { // Spectral Band Replication
-    return frameInfo.sbr; // NO_SBR           0 /* no SBR used in this file */
+    return m_frameInfo.sbr; // NO_SBR           0 /* no SBR used in this file */
                           // SBR_UPSAMPLED    1 /* upsampled SBR used */
                           // SBR_DOWNSAMPLED  2 /* downsampled SBR used */
                           // NO_SBR_UPSAMPLED 3 /* no SBR used, but file is upsampled by a factor 2 anyway */
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 void AACDecoder::createAudioSpecificConfig(uint8_t* config, uint8_t audioObjectType, uint8_t samplingFrequencyIndex, uint8_t channelConfiguration) {
     config[0] = (audioObjectType << 3) | (samplingFrequencyIndex >> 1);
     config[1] = (samplingFrequencyIndex << 7) | (channelConfiguration << 3);
 }
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 // extern uint8_t NeaacDecoder::get_sr_index(const uint32_t samplerate);
-//----------------------------------------------------------------------------------------------------------------------
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
--- src/aac_decoder/aac_decoder.h
+++ src/aac_decoder/aac_decoder.h
@@ -3,8 +3,7 @@
  *  faad2 - ESP32 adaptation
  *  Created on: 12.09.2023
  *  Updated on: 13.08.2024
-*/
-
+ */
 
 #pragma once
 
@@ -13,64 +12,61 @@
 
 #pragma GCC diagnostic warning "-Wunused-function"
 
-
 class AACDecoder : public Decoder {
 
-public:
+  public:
     AACDecoder(Audio& audioRef);
-    ~AACDecoder() {reset();}
-    bool             init() override;
-    void             clear() override;
-    void             reset() override;
-    bool             isValid() override;
-    int32_t          findSyncWord(uint8_t* buf, int32_t nBytes) override;
-    uint8_t          getChannels() override;
-    uint32_t         getSampleRate() override;
-    uint32_t         getOutputSamples();
-    uint8_t          getBitsPerSample() override;
-    uint32_t         getBitRate() override;
-    uint32_t         getAudioDataStart() override;
-    uint32_t         getAudioFileDuration() override;
-    const char*      getStreamTitle() override;
-    const char*      getErrorMessage(int8_t err) override;
-    int32_t          decode(uint8_t* inbuf, int32_t* bytesLeft, int16_t* outbuf) override;
-    void             setRawBlockParams(uint8_t channels, uint32_t sampleRate, uint8_t BPS, uint32_t tsis, uint32_t AuDaLength) override;
+    ~AACDecoder() { reset(); }
+    bool                  init() override;
+    void                  clear() override;
+    void                  reset() override;
+    bool                  isValid() override;
+    int32_t               findSyncWord(uint8_t* buf, int32_t nBytes) override;
+    uint8_t               getChannels() override;
+    uint32_t              getSampleRate() override;
+    uint32_t              getOutputSamples();
+    uint8_t               getBitsPerSample() override;
+    uint32_t              getBitRate() override;
+    uint32_t              getAudioDataStart() override;
+    uint32_t              getAudioFileDuration() override;
+    const char*           getStreamTitle() override;
+    const char*           getErrorMessage(int8_t err) override;
+    int32_t               decode(uint8_t* inbuf, int32_t* bytesLeft, int16_t* outbuf) override;
+    void                  setRawBlockParams(uint8_t channels, uint32_t sampleRate, uint8_t BPS, uint32_t tsis, uint32_t AuDaLength) override;
     std::vector<uint32_t> getMetadataBlockPicture() override;
-    const char*      arg1() override;
-    const char*      arg2() override;
-    virtual int32_t  val1() override; // Paramertric Stereo
-    virtual int32_t  val2() override; // SBR
+    const char*           arg1() override;
+    const char*           arg2() override;
+    virtual int32_t       val1() override; // Paramertric Stereo
+    virtual int32_t       val2() override; // SBR
 
-private:
-    Audio& audio;
+  private:
+    Audio&       audio;
     ps_ptr<char> m_arg1;
-    void createAudioSpecificConfig(uint8_t* config, uint8_t audioObjectType, uint8_t samplingFrequencyIndex, uint8_t channelConfiguration);
+    void         createAudioSpecificConfig(uint8_t* config, uint8_t audioObjectType, uint8_t samplingFrequencyIndex, uint8_t channelConfiguration);
 
-NeAACDecHandle hAac;
-NeAACDecFrameInfo frameInfo;
-NeAACDecConfigurationPtr conf;
-const uint8_t  SYNCWORDH = 0xff; /* 12-bit syncword */
-const uint8_t  SYNCWORDL = 0xf0;
-bool f_decoderIsInit = false;
-bool f_firstCall = false;
-bool f_setRaWBlockParams = false;
-uint32_t aacSamplerate = 0;
-uint8_t aacChannels = 0;
-uint8_t aacProfile = 0;
-uint16_t validSamples = 0;
-clock_t before;
-float compressionRatio = 1;
-mp4AudioSpecificConfig* mp4ASC;
-std::unique_ptr<NeaacDecoder> neaacdec;
+    NeAACDecHandle                m_hAac;
+    NeAACDecFrameInfo             m_frameInfo;
+    NeAACDecConfigurationPtr      m_conf;
+    const uint8_t                 SYNCWORDH = 0xff; /* 12-bit syncword */
+    const uint8_t                 SYNCWORDL = 0xf0;
+    bool                          m_f_decoderIsInit = false;
+    bool                          m_f_firstCall = false;
+    bool                          m_f_setRaWBlockParams = false;
+    uint32_t                      m_aacSamplerate = 0;
+    uint8_t                       m_aacChannels = 0;
+    uint8_t                       m_aacProfile = 0;
+    uint16_t                      m_validSamples = 0;
+    float                         m_compressionRatio = 1;
+    std::unique_ptr<NeaacDecoder> m_neaacdec;
 
-struct AudioSpecificConfig {
-    uint8_t audioObjectType;
-    uint8_t samplingFrequencyIndex;
-    uint8_t channelConfiguration;
-};
+    struct AudioSpecificConfig {
+        uint8_t audioObjectType;
+        uint8_t samplingFrequencyIndex;
+        uint8_t channelConfiguration;
+    };
 
-//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// Macro for comfortable calls
+// ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+//  Macro for comfortable calls
 #define AAC_LOG_ERROR(fmt, ...)   Audio::AUDIO_LOG_IMPL(1, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
 #define AAC_LOG_WARN(fmt, ...)    Audio::AUDIO_LOG_IMPL(2, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
 #define AAC_LOG_INFO(fmt, ...)    Audio::AUDIO_LOG_IMPL(3, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
