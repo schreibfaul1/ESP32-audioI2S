172121fcabdbee7f161d86fdc21884ec617a9653
schreibfaul1 <schreibfaul1@t-online.de>
2025-11-15 10:52:28 +0100

    inBufferStatus

--- src/Audio.h
+++ src/Audio.h
@@ -144,6 +144,7 @@ class Audio {
     uint32_t         inBufferFilled();  // returns the number of stored bytes in the inputbuffer
     uint32_t         inBufferFree();    // returns the number of free bytes in the inputbuffer
     uint32_t         getInBufferSize(); // returns the size of the inputbuffer in bytes
+    void             inBufferStatus(){InBuff.showStatus();}
     void             setTone(int8_t gainLowPass, int8_t gainBandPass, int8_t gainHighPass);
     void             setI2SCommFMT_LSB(bool commFMT);
     int              getCodec() { return m_codec; }
