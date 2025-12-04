cddf33c4191c28529c436e4f7c2ed042b5e310a4
schreibfaul1 <schreibfaul1@t-online.de>
2025-12-04 22:17:54 +0100

    v3.4.4

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -4,8 +4,8 @@
 
     Created on: 28.10.2018                                                                                                  */
 char audioI2SVers[] = "\
-    Version 3.4.3ze                                                                                                                              ";
-/*  Updated on: 27.11.2025
+    Version 3.4.4                                                                                                                              ";
+/*  Updated on: 04.12.2025
 
     Author: Wolle (schreibfaul1)
     Audio library for ESP32, ESP32-S3 or ESP32-P4
