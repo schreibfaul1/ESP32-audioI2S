43df762c927a73e397c0bbf6652f236609c16a74
schreibfaul1 <schreibfaul1@t-online.de>
2026-02-22 15:11:02 +0100

    #1256 setOutput48KHz instead #define SR_48K

--- src/Audio.h
+++ src/Audio.h
@@ -3,8 +3,6 @@
  *
  */
 
-#define SR_48K // only for I2S devices (DAC, BT etc.) that require 48KHz
-
 #pragma once
 #pragma GCC optimize("Ofast")
 #include "audiolib_structs.hpp"
@@ -125,6 +123,7 @@ class Audio {
     void             loop();
     uint32_t         stopSong();
     void             forceMono(bool m);
+    void             setOutput48KHz(bool f48);
     void             setBalance(float balance = 0.0f);
     void             setVolumeSteps(uint8_t steps);
     uint8_t          getVolumeSteps();
@@ -420,6 +419,7 @@ class Audio {
     bool           m_f_tts = false;                 // text to speech
     bool           m_f_ogg = false;                 // OGG stream
     bool           m_f_forceMono = false;           // if true stereo -> mono
+    bool           m_f_output48KHz = false;         // resample to I2S 48000Hz
     bool           m_f_rtsp = false;                // set if RTSP is used (m3u8 stream)
     bool           m_f_m3u8data = false;            // used in processM3U8entries
     bool           m_f_continue = false;            // next m3u8 chunk is available
